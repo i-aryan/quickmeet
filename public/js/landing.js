8a6c98db1c45dbf63c5288e69fb3ce4943b91690
Aryan Verma <aryanv2001@gmail.com>
2020-09-19 22:47:04 +0530

    UI change for landing page

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,5 +1,12 @@
 const createButton = document.querySelector("#createroom");
-const nameField = document.querySelector("#username");
+const videoCont = document.querySelector('.video-self');
+
+const mediaConstraints = { video: true};
+
+navigator.mediaDevices.getUserMedia(mediaConstraints)
+.then(localstream=>{
+    videoCont.srcObject = localstream;
+})
 
 function uuidv4() {
     return 'xxyxyxxyx'.replace(/[xy]/g, function (c) {
@@ -8,8 +15,24 @@ function uuidv4() {
     });
 }
 
+const createroomtext = 'Creating Room...';
+
 createButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const name = nameField.value;
-    location.href = `/room.html?name=${name}&room=${uuidv4()}`;
+    createButton.disabled = true;
+    createButton.innerHTML ='Creating Room';
+    createButton.classList = 'createroom-clicked';
+
+    setInterval(()=>{
+        if(createButton.innerHTML < createroomtext){
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length+1);
+        }
+        else{
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length - 3);
+        }
+    }, 500);
+
+    //const name = nameField.value;
+    //location.href = `/room.html?name=${name}&room=${uuidv4()}`;
 });
+
