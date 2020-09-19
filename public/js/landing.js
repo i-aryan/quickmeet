8d557cfea49568c0ff84cad677a42ee7dfc0de37
Aryan Verma <aryanv2001@gmail.com>
2020-09-20 01:06:23 +0530

    added functionality to mic/audio icons

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,12 +1,19 @@
 const createButton = document.querySelector("#createroom");
 const videoCont = document.querySelector('.video-self');
+const codeCont = document.querySelector('#roomcode');
+const joinBut = document.querySelector('#joinroom');
+const mic = document.querySelector('#mic');
+const cam = document.querySelector('#webcam');
 
-const mediaConstraints = { video: true};
+let micAllowed = 1;
+let camAllowed = 1;
+
+let mediaConstraints = { video: true, audio: true };
 
 navigator.mediaDevices.getUserMedia(mediaConstraints)
-.then(localstream=>{
-    videoCont.srcObject = localstream;
-})
+    .then(localstream => {
+        videoCont.srcObject = localstream;
+    })
 
 function uuidv4() {
     return 'xxyxyxxyx'.replace(/[xy]/g, function (c) {
@@ -20,14 +27,14 @@ const createroomtext = 'Creating Room...';
 createButton.addEventListener('click', (e) => {
     e.preventDefault();
     createButton.disabled = true;
-    createButton.innerHTML ='Creating Room';
+    createButton.innerHTML = 'Creating Room';
     createButton.classList = 'createroom-clicked';
 
-    setInterval(()=>{
-        if(createButton.innerHTML < createroomtext){
-            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length+1);
+    setInterval(() => {
+        if (createButton.innerHTML < createroomtext) {
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length + 1);
         }
-        else{
+        else {
             createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length - 3);
         }
     }, 500);
@@ -36,3 +43,68 @@ createButton.addEventListener('click', (e) => {
     //location.href = `/room.html?name=${name}&room=${uuidv4()}`;
 });
 
+joinBut.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (codeCont.value.trim() == "") {
+        codeCont.classList.add('roomcode-error');
+        return;
+    }
+})
+
+codeCont.addEventListener('change', (e) => {
+    e.preventDefault();
+    if (codeCont.value.trim() !== "") {
+        codeCont.classList.remove('roomcode-error');
+        return;
+    }
+})
+
+cam.addEventListener('click', () => {
+    if (camAllowed) {
+        mediaConstraints = { video: false, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "nodevice";
+        cam.innerHTML = `<i class="fas fa-video-slash"></i>`;
+        camAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: true, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "device";
+        cam.innerHTML = `<i class="fas fa-video"></i>`;
+        camAllowed = 1;
+    }
+})
+
+mic.addEventListener('click', () => {
+    if (micAllowed) {
+        mediaConstraints = { video: camAllowed ? true : false, audio: false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.classList = "nodevice";
+        mic.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+        micAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: camAllowed ? true : false, audio: true };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.innerHTML = `<i class="fas fa-microphone"></i>`;
+        mic.classList = "device";
+        micAllowed = 1;
+    }
+})
