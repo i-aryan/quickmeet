a197a20515a98a20562adac35e172c1e29e9cb70
Aryan Verma <aryanv2001@gmail.com>
2021-07-09 15:48:35 +0530

    added screenshare

--- public/js/room.js
+++ public/js/room.js
@@ -402,6 +402,57 @@ function handleVideoAnswer(answer, sid) {
     connections[sid].setRemoteDescription(ans);
 }
 
+//Thanks to (https://github.com/miroslavpejic85) for ScreenShare Code
+
+screenShareButt.addEventListener('click', () => {
+    screenShareToggle();
+});
+let screenshareEnabled = false;
+function screenShareToggle() {
+    let screenMediaPromise;
+    if (!screenshareEnabled) {
+        if (navigator.getDisplayMedia) {
+            screenMediaPromise = navigator.getDisplayMedia({ video: true });
+        } else if (navigator.mediaDevices.getDisplayMedia) {
+            screenMediaPromise = navigator.mediaDevices.getDisplayMedia({ video: true });
+        } else {
+            screenMediaPromise = navigator.mediaDevices.getUserMedia({
+                video: { mediaSource: "screen" },
+            });
+        }
+    } else {
+        screenMediaPromise = navigator.mediaDevices.getUserMedia({ video: true });
+    }
+    screenMediaPromise
+        .then((myscreenshare) => {
+            screenshareEnabled = !screenshareEnabled;
+            for (let key in connections) {
+                const sender = connections[key]
+                    .getSenders()
+                    .find((s) => (s.track ? s.track.kind === "video" : false));
+                sender.replaceTrack(myscreenshare.getVideoTracks()[0]);
+            }
+            myscreenshare.getVideoTracks()[0].enabled = true;
+            const newStream = new MediaStream([
+                myscreenshare.getVideoTracks()[0], 
+            ]);
+            myvideo.srcObject = newStream;
+            myvideo.muted = true;
+            mystream = newStream;
+            screenShareButt.innerHTML = (screenshareEnabled 
+                ? `<i class="fas fa-desktop"></i><span class="tooltiptext">Stop Share Screen</span>`
+                : `<i class="fas fa-desktop"></i><span class="tooltiptext">Share Screen</span>`
+            );
+            myscreenshare.getVideoTracks()[0].onended = function() {
+                if (screenshareEnabled) screenShareToggle();
+            };
+        })
+        .catch((e) => {
+            alert("Unable to share screen:" + e.message);
+            console.error(e);
+        });
+}
+
 socket.on('video-offer', handleVideoOffer);
 
 socket.on('new icecandidate', handleNewIceCandidate);
